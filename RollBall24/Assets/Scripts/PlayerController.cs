28fa42a2508c6ed5b31585115227cb259e823c63
Mushy2408 <127207722+Mushy2408@users.noreply.github.com>
2024-12-02 09:43:42 -0500

    done

--- RollBall24/Assets/Scripts/PlayerController.cs
+++ RollBall24/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;// the player speed
 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;//rigidbody of the player
+
+    private int count;
 
     private float movementX;//the X and Y axes 
     private float movementY;
@@ -16,6 +20,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementvalue)
@@ -25,6 +34,14 @@ public class PlayerController : MonoBehaviour
         movementX = movementvector.x;
         movementY = movementvector.y;
     }
+    void SetCountText()
+    {
+        countText.text = "count:" + count.ToString();
+        if (count > 11)
+        {
+            winTextObject.SetActive (true);
+        }
+    }
 
 
     private void FixedUpdate()
@@ -32,5 +49,22 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX,0.0f,movementY);
 
         rb.AddForce(movement * speed);
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+           other.gameObject.SetActive(false);
+            count= count + 1;
+
+            SetCountText();
+        }
+       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if
     }
 }
